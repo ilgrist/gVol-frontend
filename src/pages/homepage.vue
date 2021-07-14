3169f928ea8910fb45796bf1c4fc955e82f4b4d0
Barak Vadei <barackv95@gmail.com>
2021-07-14 15:51:23 +0300

    add homepage short list

--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
     <h1>Welcome to gVol!</h1>
+    <short-list />
   </div>
 </template>
 
 <script>
+import shortList from '@/cmps/homepage-cmps/short-volOp-list.vue'
 export default {
   name: "Home",
-  components: {},
+  components: {
+    shortList,
+  },
 };
 </script>

b9f1ab065645e5ea9f0618f42be7ed0d6fc49618
RV <racheli.vardi@gmail.com>
2021-07-15 16:24:56 +0300

    profile page

--- src/cmps/profile-cmps/vol-reviews.vue
+++ src/cmps/profile-cmps/vol-reviews.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="details-reviews">
-    <h3>Reviews</h3>
+    <header class="review-header">
+      <h3>{{ vol.reviews.length }} Review(s) relating to {{ vol.title }}:</h3>
+      <button class="review-btn">Add a Review</button>
+    </header>
     <ul v-for="review in vol.reviews" :key="review._id">
       <li>{{ review.createdBy }}: {{ review.txt }}</li>
     </ul>

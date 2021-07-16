f40c8613b97b522429fe0f42f06f771274d682e2
RV <racheli.vardi@gmail.com>
2021-07-16 22:52:06 +0300

    header fixes

--- src/cmps/main-layout-cmps/main-header.vue
+++ src/cmps/main-layout-cmps/main-header.vue
@@ -9,13 +9,13 @@
     <!-- src="https://res.cloudinary.com/dzuqvua7k/image/upload/v1626281313/volApp/logo3_wuazzc.png" -->
     <nav id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/volApp">Explore</router-link>
+      <router-link to="/volApp">Explore</router-link> |
       <!-- <router-link to="/user/:_id">Login/Loutout</router-link> -->
       <!-- <p v-if="!isLoggedIn">Hello Guest</p> -->
       <!-- <router-link :to="/user/" +loggedinUser._id>
         | Profile/Logout</router-link
       > -->
-      <router-link to="/login"> | Welcome Guest</router-link>
+      <router-link to="/login"> Welcome Guest</router-link>
     </nav>
   </header>
 </template>

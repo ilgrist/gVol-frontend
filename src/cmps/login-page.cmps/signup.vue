581c6b4db6d9637c526f187edc574b2f481405be
RV <racheli.vardi@gmail.com>
2021-07-21 10:22:24 +0300

    fixed signup

--- src/cmps/login-page.cmps/signup.vue
+++ src/cmps/login-page.cmps/signup.vue
@@ -1,40 +1,50 @@
 <template>
   <section>
-      <h2>Sign-up</h2>
-      <div class="label-input">
-        <label for="fullnme">
-          <p>Fullname:</p>
-          <input v-model="newUser.fullname" id="fullnme" type="text" />
-        </label>
-        <label for="username">
-          <p>Username:</p>
-          <input v-model="newUser.username" id="username" type="text" />
-        </label>
-        <label for="password">
-          <p>Password:</p>
-          <input v-model="newUser.password" id="password" type="password" />
-        </label>
-        <label class="skills" for="skills">
-          <p>Skills:</p>
-          <select name="" v-model="newUser.skills" id="skills">
-            <option v-for="skill in skills" :key="skill" :value="skill">
-              {{ skill }}
-            </option>
-          </select>
-        </label>
-        <label class="profile-img" for="img"
-          >Profile img:
-          <img
-            src="https://res.cloudinary.com/dzuqvua7k/image/upload/v1626419180/volApp/icons/account_circle_black_24dp_meg4mh.svg"
-          />
-          <input id="img" type="file" @change="handleFile" />
-        </label>
-        <img v-if="isSelectImg" class="img-prev" :src="newUser.imgUrl">
-      </div>
-      <div class="signup-btn">
-        <button @click="signup">Sign-up</button>
-        <button @click="back">Back</button>
-      </div>
+    <h2>Sign-up</h2>
+    <div class="label-input">
+      <label for="fullnme">
+        <p>Fullname:</p>
+        <input v-model="newUser.fullname" id="fullnme" type="text" />
+      </label>
+      <label for="username">
+        <p>Username:</p>
+        <input v-model="newUser.username" id="username" type="text" />
+      </label>
+      <label for="password">
+        <p>Password:</p>
+        <input v-model="newUser.password" id="password" type="password" />
+      </label>
+      <label class="skills" for="skills">
+        <p>Skills:</p>
+        <el-select
+          placeholder="Existing Skills"
+          name=""
+          multiple
+          v-model="newUser.skills"
+          id="skills"
+        >
+          <el-option
+            v-for="skill in skills"
+            :key="skill.value"
+            :label="skill.label"
+            :value="skill.value"
+          >
+          </el-option>
+        </el-select>
+      </label>
+      <label class="profile-img" for="img"
+        >Profile img:
+        <img
+          src="https://res.cloudinary.com/dzuqvua7k/image/upload/v1626419180/volApp/icons/account_circle_black_24dp_meg4mh.svg"
+        />
+        <input id="img" type="file" @change="handleFile" />
+      </label>
+      <img v-if="isSelectImg" class="img-prev" :src="newUser.imgUrl" />
+    </div>
+    <div class="signup-btn">
+      <button @click="signup">Sign-up</button>
+      <button @click="back">Back</button>
+    </div>
   </section>
 </template>
 
@@ -42,34 +52,60 @@
 import { uploadImg } from "../../services/img-upload.service.js";
 
 export default {
+  data() {
+    return {
+      isSelectImg: false,
+      newUser: {
+        fullname: "",
+        username: "",
+        password: "",
+        skills: [],
+        imgUrl: "",
+      },
 
-    data(){
-        return{
-          isSelectImg:false,
-          newUser: {
-                fullname: "",
-                username: "",
-                password: "",
-                skills: "",
-                imgUrl: "",
-                },
-                skills: ["-", "teaching", "translating", "designing"],
-        }
+      skills: [
+        {
+          value: "dreaming",
+          label: "Dreaming",
+        },
+        {
+          value: "building",
+          label: "Building",
+        },
+        {
+          value: "translating",
+          label: "Translating",
+        },
+        {
+          value: "hugging",
+          label: "Hugging",
+        },
+        {
+          value: "saving",
+          label: "Saving",
+        },
+      ],
+      // skills: ["-", "teaching", "translating", "designing"],
+    };
+  },
+  methods: {
+    pushSkill(skill) {
+      console.log("file: signup.vue ~ line 60 ~ skill", skill);
+      this.newUser.skills.push(skill);
     },
-    methods: {
-        signup() {
-          const newUserCopy =JSON.parse(JSON.stringify(this.newUser))
-            this.$emit('signup', newUserCopy)
-        },
-        async handleFile(ev) {
-        const file = ev.target.files[0];
-        const res = await uploadImg(ev);
-        this.newUser.imgUrl = res.url;
-        this.isSelectImg = true
-        },
-        back() {
-        this.$emit('back')
-        },
-        }
-}
+    signup() {
+      const newUserCopy = JSON.parse(JSON.stringify(this.newUser));
+      this.$emit("signup", newUserCopy);
+    },
+    async handleFile(ev) {
+      const file = ev.target.files[0];
+      const res = await uploadImg(ev);
+      this.newUser.imgUrl = res.url;
+      this.isSelectImg = true;
+    },
+    back() {
+      this.$emit("back");
+    },
+  },
+};
 </script>

b1c8a2d26b595628502c07397b824203ab2bf8dc
RV <racheli.vardi@gmail.com>
2021-07-22 16:55:58 +0300

    typo change

---
+++ src/cmps/profile-cmps/vol-map.vue
@@ -0,0 +1,17 @@
+<template>
+  <GmapMap :center="{ lat: 10, lng: 10 }" :zoom="7" map-type-id="terrain">
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="england"
+      :clickable="true"
+      :draggable="true"
+      @click="center = m.position"
+    />
+  </GmapMap>
+</template>
+
+<script>
+export default {};
+</script>
+
